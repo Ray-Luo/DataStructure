7da40091199323eeb56dbe89fad18e19bc7ef560
RAy-Luo <leiuo.ray@gmail.com>
2017-05-20 23:01:13 -0500

    added AST and BinarySearchTree

---
+++ Chap6/AbstractSyntaxTree.py
@@ -0,0 +1,40 @@
+class TimesNode:
+
+  def __init__(self, left, right):
+    self.left = left
+    self.right = right
+
+  def eval(self):
+    return self.left.eval() * self.right.eval()
+
+  def inorder(self):
+    return "(" + self.left.inorder() + " * " + self.right.inorder() + ")"
+
+class PlusNode:
+
+  def __init__(self, left, right):
+    self.left = left
+    self.right = right
+
+  def eval(self):
+    return self.left.eval() + self.right.eval()
+
+class NumNode:
+  
+  def __init__(self, num):
+    self.num = num
+
+  def eval(self):
+    return self.num
+
+def main():
+  x = NumNode(5)
+  y = NumNode(4)
+  p = PlusNode(x,y)
+  t = TimesNode(p, NumNode(6))
+  root = PlusNode(t, NumNode(3))
+  print(root.eval())
+
+if __name__ == "__main__":
+  main()
+
